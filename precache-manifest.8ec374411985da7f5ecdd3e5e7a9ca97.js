23d778f17d470f0e912dc86d9aeb8d1bb53c9a40
Viaje9 <h99y5246cv@gmail.com>
2021-02-21 19:43:44 +0800

    test

---
+++ precache-manifest.8ec374411985da7f5ecdd3e5e7a9ca97.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7278a4ca39179c7bf3",
+    "url": "/css/app.3d94631b.css"
+  },
+  {
+    "revision": "434507410eae590f3539",
+    "url": "/css/chunk-vendors.24bf1742.css"
+  },
+  {
+    "revision": "92d99f29f65e2d2c79e8d4a2ae23cfa8",
+    "url": "/img/account.92d99f29.svg"
+  },
+  {
+    "revision": "2ca5f0e8e9aff0cea8aa4a5a2b94f032",
+    "url": "/img/avatar.2ca5f0e8.svg"
+  },
+  {
+    "revision": "0a068a4d920abe63b6b555c57f08e80a",
+    "url": "/img/friends.0a068a4d.svg"
+  },
+  {
+    "revision": "fe352adfdd3f56791b385728d0503b2f",
+    "url": "/img/lock.fe352adf.svg"
+  },
+  {
+    "revision": "599134614421367086c024d95486ec46",
+    "url": "/img/name.59913461.svg"
+  },
+  {
+    "revision": "895d3b6270eb72ba9b3258a83292901b",
+    "url": "/img/search.895d3b62.svg"
+  },
+  {
+    "revision": "2b9365ffcff483af859359251e18f16a",
+    "url": "/img/settings.2b9365ff.svg"
+  },
+  {
+    "revision": "3e88bc7f0b308a7360366cc08c2b0e02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c7278a4ca39179c7bf3",
+    "url": "/js/app.72fd0156.js"
+  },
+  {
+    "revision": "434507410eae590f3539",
+    "url": "/js/chunk-vendors.7682a155.js"
+  },
+  {
+    "revision": "9da1f908ab1b1d744e1266afe5021564",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
